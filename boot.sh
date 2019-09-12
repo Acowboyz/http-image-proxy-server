f47e281fe6f3f5ac9d14da746903f4ccd3ccad9e
Felix Lin <its.acowboyz@gmail.com>
2019-09-12 14:53:40 +0800

    init the db by schema.sql

--- boot.sh
+++ boot.sh
@@ -3,4 +3,7 @@
 host=127.0.0.1
 port=5000
 
-gunicorn -k gevent -w 1 -b $host:$port http_image_proxy_server:app
+cat schema.sql | sqlite3 data.db
+gunicorn -k gevent --worker-connections 100 -b ${host}:${port} http_image_proxy_server:app
+#gunicorn http_image_proxy_server:aioapp -k aiohttp.worker.GunicornWebWorker --worker-connections 100 -b ${host}:${port}
+#gunicorn -w 1 -b ${host}:${port} http_image_proxy_server:app
