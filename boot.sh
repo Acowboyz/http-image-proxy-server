cd409698b78cf1f18c0cb39d13ee5cf8e0a99c95
Felix Lin <its.acowboyz@gmail.com>
2019-09-16 05:48:31 +0800

    update the boot.sh

--- boot.sh
+++ boot.sh
@@ -3,7 +3,6 @@
 host=127.0.0.1
 port=5000
 
-cat schema.sql | sqlite3 data.db
-gunicorn -k gevent --worker-connections 100 -b ${host}:${port} http_image_proxy_server:app
+gunicorn -k gevent --worker-connections 100 -b ${host}:${port} app:app
 #gunicorn http_image_proxy_server:aioapp -k aiohttp.worker.GunicornWebWorker --worker-connections 100 -b ${host}:${port}
 #gunicorn -w 1 -b ${host}:${port} http_image_proxy_server:app
